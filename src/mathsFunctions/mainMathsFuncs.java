2d9d3eec2386980fb81395a099b45fbf94c987f5
Lawrence Pinto <pinto120474@gmail.com>
2021-12-11 15:01:27 +0530

    Chemistry Subject Added

--- src/mathsFunctions/mainMathsFuncs.java
+++ src/mathsFunctions/mainMathsFuncs.java
@@ -1,5 +1,7 @@
 package mathsFunctions;
 
+import javax.security.auth.Subject;
+
 public class mainMathsFuncs {
     public static void main(String[] args) {
 
@@ -9,5 +11,11 @@ public class mainMathsFuncs {
         mathsCode.deductNumbers(40, 21);
         mathsCode.divideNumbers(20, 10);
         mathsCode.multiplyNumbers(5, 10);
+
+        Subjects sbMaths = new Subjects("Maths", "Molly", 90, 35);
+        Subjects sbChemics = new Subjects("Chemistry", "John", 50, 40);
+
+        System.out.println(sbMaths);
+        System.out.println(sbChemics);
     }
 }
